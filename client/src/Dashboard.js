9518d536aa97ec84d62dc19d8eab8d2ba026f4f4
Tokhirbek <karamaov.tohirbek@gmail.com>
2022-04-14 22:01:02 +0300

    Search musics

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,14 +1,52 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Container, Form } from 'react-bootstrap';
+import SpotifyWebApi from 'spotify-web-api-node';
 import useAuth from './useAuth';
+import TrackSearchResult from './TrackSearchResult';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: 'eb307afae0d74941b50dbce32a536d1a',
+});
 
 // eslint-disable-next-line react/prop-types
 export default function Dashboard({ code }) {
   // eslint-disable-next-line no-unused-vars
   const accessToken = useAuth(code);
   const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
   // eslint-disable-next-line react/jsx-filename-extension
+
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return
+
+    let cancel = false;
+    spotifyApi.searchTracks(search).then((res) => {
+      if (cancel) return
+      setSearchResults(res.body.tracks.items.map((track) => {
+        const smallestalbumImage = track.album.images.reduce((smallest, image) => {
+          if (image.height < smallest.height) return image;
+          return smallest;
+        }, track.album.images[0]);
+
+        return {
+          artist: track.artists[0].name,
+          title: track.name,
+          uri: track.uri,
+          albumUrl: smallestalbumImage.url,
+        };
+      }));
+    });
+
+    return () => cancel = true;
+  }, [search, accessToken]);
+
   return (
     // eslint-disable-next-line react/jsx-filename-extension
     <Container className="d-flex flex-column py-3" style={{ height: '100vh' }}>
@@ -19,7 +57,9 @@ export default function Dashboard({ code }) {
         onChange={(e) => setSearch(e.target.value)}
       />
       <div className="flex-grow-1 my-2" style={{ overflowY: 'auto' }}>
-        Песни
+        {searchResults.map(track => (
+          <TrackSearchResult track={track} key={track.uri} />
+        ))}
       </div>
       <div>Bottom</div>
     </Container>
---
+++ client/src/TrackSearchResult.js
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
+
+export default function TrackSearchResult({ track, chooseTrack }) {
+  function handlePlay() {
+    chooseTrack(track);
+  }
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <div
+      className="d-flex m-3 align-items-center"
+      style={{ cursor: 'pointer' }}
+      onClick={handlePlay}
+      >
+      <img src={track.albumUrl} style={{ height: '64px', width: '64px' }} />
+      <div className="ml-3">
+        <div>{track.title}</div>
+        <div className="text-muted">{track.artist}</div>
+      </div>
+    </div>
+  );
+}
